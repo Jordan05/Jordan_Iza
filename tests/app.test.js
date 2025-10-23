fbf123b76eb6fbae280cdc52e504b3ff5c0c36f2
Jordaniza05 <jis.iza@yavirac.edu.ec>
2025-10-23 19:33:53 +0200

    work flow ok

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,13 +5,13 @@ describe('Pruebas de la aplicación', () => {
   test('Debe responder con mensaje de inicio', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    //expect(res.text).toContain('App funcionando');
-    expect(res.text).toContain('Fallo intencional'); // Esto hará que el test falle
+    expect(res.text).toContain('App funcionando');
+    //expect(res.text).toContain('Fallo intencional'); // Esto hará que el test falle
   });
 
   test('Debe sumar correctamente dos números', async () => {
     const res = await request(app).get('/suma?a=2&b=3');
-    //expect(res.body.resultado).toBe(5);
-    expect(res.body.resultado).toBe(10); // Fallará porque la suma real es 5
+    expect(res.body.resultado).toBe(5);
+    //expect(res.body.resultado).toBe(10); // Fallará porque la suma real es 5
   });
 });
